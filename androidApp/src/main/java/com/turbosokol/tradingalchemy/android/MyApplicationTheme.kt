308d1ea373bf7c3c176308fc82b0b4bc98a192d8
ijirak <119900896+ijirak@users.noreply.github.com>
2020-05-06 20:54:48 +0000

    Update gitignore file

--- androidApp/src/main/java/com/turbosokol/tradingalchemy/android/MyApplicationTheme.kt
+++ androidApp/src/main/java/com/turbosokol/tradingalchemy/android/MyApplicationTheme.kt
@@ -31,3 +31,26 @@ fun MyApplicationTheme(
         lightColors(
             primary = Color(0xFF6200EE),
             primaryVariant = Color(0xFF3700B3),
+            secondary = Color(0xFF03DAC5)
+        )
+    }
+    val typography = Typography(
+        body1 = TextStyle(
+            fontFamily = FontFamily.Default,
+            fontWeight = FontWeight.Normal,
+            fontSize = 16.sp
+        )
+    )
+    val shapes = Shapes(
+        small = RoundedCornerShape(4.dp),
+        medium = RoundedCornerShape(4.dp),
+        large = RoundedCornerShape(0.dp)
+    )
+
+    MaterialTheme(
+        colors = colors,
+        typography = typography,
+        shapes = shapes,
+        content = content
+    )
+}
