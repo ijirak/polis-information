a858e5824fef4d1016a3b861d7dce29ff6678a1e
ijirak <119900896+ijirak@users.noreply.github.com>
2020-05-06 05:54:09 +0000

    No, I'm not.

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -36,3 +36,9 @@ dependencies {
     implementation(project(":sharedLogic"))
     implementation(project(":sharedUI"))
     implementation("androidx.compose.ui:ui:1.2.1")
+    implementation("androidx.compose.ui:ui-tooling:1.2.1")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.2.1")
+    implementation("androidx.compose.foundation:foundation:1.2.1")
+    implementation("androidx.compose.material:material:1.2.1")
+    implementation("androidx.activity:activity-compose:1.5.1")
+}
---
+++ androidApp/src/main/java/com/turbosokol/tradingalchemy/android/MainActivity.kt
@@ -0,0 +1,17 @@
+
+package com.turbosokol.tradingalchemy.android
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.turbosokol.sharedfront.ui.MainView
+import com.turbosokol.sharedfront.util.ApplicationViewModel
+import org.koin.java.KoinJavaComponent.inject
