83d7047a91480edebace648dd4cdf18dd1a79dd3
ijirak <119900896+ijirak@users.noreply.github.com>
2020-05-14 22:54:33 +0000

    zsmalloc: Fix CPU hotplug callback registration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
 plugins {
     //trick: for the same plugin versions in all sub-modules
-    id("com.android.application").version("7.4.0-rc0
+    id("com.android.application").version("7.4.0-rc03").apply(false)
+    id("com.android
