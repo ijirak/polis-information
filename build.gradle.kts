0afb3fd5aecd06f633d894249589cbc02beed537
ijirak <119900896+ijirak@users.noreply.github.com>
2020-05-15 07:14:09 +0000

    MONDRIAN: Oops!

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
     //trick: for the same plugin versions in all sub-modules
     id("com.android.application").version("7.4.0-rc03").apply(false)
-    id("com.android
+    id("com.android.library").version("7.4.0-rc03").apply(fals
