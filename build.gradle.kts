1cc693a582ddc329779c77efa474de4ce182a990
ijirak <119900896+ijirak@users.noreply.github.com>
2020-05-18 05:52:43 +0000

    description change

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,7 @@ plugins {
     id("com.android.application").version("7.4.0-rc03").apply(false)
     id("com.android.library").version("7.4.0-rc03").apply(false)
     kotlin("android").version("1.7.10").apply(false)
-    kotlin("multiplatform").version("1.7.10").ap
+    kotlin("multiplatform").version("1.7.10").apply(false)
+}
+
+tasks.registe
