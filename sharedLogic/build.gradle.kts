dcf9c339ad1a5d4920fa98dba9fd293e35af140f
ijirak <119900896+ijirak@users.noreply.github.com>
2022-01-23 13:41:01 +0000

    Removing unnecessary imports

--- sharedLogic/build.gradle.kts
+++ sharedLogic/build.gradle.kts
@@ -36,3 +36,46 @@ kotlin {
             }
         }
         val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+        val androidMain by getting {
+            dependencies {
+                    implementation(libs.sqldelight.driver.android)
+                    implementation(libs.kotlinx.coroutines.android)
+                    implementation(libs.ktor.client.okhttp)
+                    implementation(libs.androidx.core)
+            }
+        }
+        val androidTest by getting
+        val iosX64Main by getting
+        val iosArm64Main by getting
+        val iosSimulatorArm64Main by getting
+        val iosMain by creating {
+            dependsOn(commonMain)
+            iosX64Main.dependsOn(this)
+            iosArm64Main.dependsOn(this)
+            iosSimulatorArm64Main.dependsOn(this)
+
+            dependencies {
+                implementation(libs.sqldelight.driver.ios)
+                implementation(libs.ktor.client.ios)
+            }
+        }
+        val iosX64Test by getting
+        val iosArm64Test by getting
+        val iosSimulatorArm64Test by getting
+        val iosTest by creating {
+            dependsOn(commonTest)
+            iosX64Test.dependsOn(this)
+            iosArm64Test.dependsOn(this)
+            iosSimulatorArm64Test.dependsOn(this)
+        }
+    }
+}
+
+android {
+    namespace = "com.turbosokol.tradingalchemy"
+    compileSdk = 33
+    defaultConfig {
