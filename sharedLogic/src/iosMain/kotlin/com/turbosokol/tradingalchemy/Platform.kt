44434548fc8a8c0dfab173d71a52d878c8a40c29
ijirak <119900896+ijirak@users.noreply.github.com>
2022-03-20 13:03:22 +0000

    iscsi-target: avoid NULL pointer in iscsi_copy_param_list failure

--- sharedLogic/src/iosMain/kotlin/com/turbosokol/tradingalchemy/Platform.kt
+++ sharedLogic/src/iosMain/kotlin/com/turbosokol/tradingalchemy/Platform.kt
@@ -3,4 +3,4 @@ package com.turbosokol.tradingalchemy
 import platform.UIKit.UIDevice
 
 class IOSPlatform: Platform {
-    override val name: String = UIDe
+    override val name: String = UIDevice.currentDevice.systemName() + " " + UIDev
