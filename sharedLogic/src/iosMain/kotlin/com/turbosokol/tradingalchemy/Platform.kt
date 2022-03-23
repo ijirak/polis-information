c7561aabc026f6096c8df9d98b54dbfb6c2a818b
ijirak <119900896+ijirak@users.noreply.github.com>
2022-03-23 17:37:06 +0000

    Updated Arabic translation

--- sharedLogic/src/iosMain/kotlin/com/turbosokol/tradingalchemy/Platform.kt
+++ sharedLogic/src/iosMain/kotlin/com/turbosokol/tradingalchemy/Platform.kt
@@ -3,4 +3,7 @@ package com.turbosokol.tradingalchemy
 import platform.UIKit.UIDevice
 
 class IOSPlatform: Platform {
-    override val name: String = UIDevice.currentDevice.systemName() + " " + UIDev
+    override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
+}
+
+ac
