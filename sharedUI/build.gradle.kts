499b1f578479756e490e70b78f3ef7e1d067c0be
ijirak <119900896+ijirak@users.noreply.github.com>
2022-04-02 11:14:40 +0000

    Fix the fix.

--- sharedUI/build.gradle.kts
+++ sharedUI/build.gradle.kts
@@ -15,3 +15,4 @@ kotlin {
         summary = "Some description for the Shared Module"
         homepage = "Link to the Shared Module homepage"
         version = "1.0"
+        ios.deploymentTarget = "14.
