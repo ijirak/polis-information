1baaca78a506925da5287a0e68975efac64b34ae
ijirak <119900896+ijirak@users.noreply.github.com>
2022-04-03 13:56:30 +0000

    net: wireless: bcmdhd: check privilege on priv cmd

--- sharedUI/build.gradle.kts
+++ sharedUI/build.gradle.kts
@@ -15,4 +15,6 @@ kotlin {
         summary = "Some description for the Shared Module"
         homepage = "Link to the Shared Module homepage"
         version = "1.0"
-        ios.deploymentTarget = "14.
+        ios.deploymentTarget = "14.1"
+        framework {
+            bas
