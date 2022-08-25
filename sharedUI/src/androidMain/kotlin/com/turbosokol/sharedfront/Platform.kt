7705d38547aae67afd5164a282401f541ee8fc31
ijirak <119900896+ijirak@users.noreply.github.com>
2022-08-25 16:24:23 +0000

    Fixed urls

--- sharedUI/src/androidMain/kotlin/com/turbosokol/sharedfront/Platform.kt
+++ sharedUI/src/androidMain/kotlin/com/turbosokol/sharedfront/Platform.kt
@@ -4,4 +4,4 @@ class AndroidPlatform : Platform {
     override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
 }
 
-actual fun getPlatform(): Pla
+actual fun getPlatform(): Platform = AndroidPlatform()
