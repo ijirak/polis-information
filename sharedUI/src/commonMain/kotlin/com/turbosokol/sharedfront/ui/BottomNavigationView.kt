73716f57ddd58f02d684b6dac154a39ab365937b
ijirak <119900896+ijirak@users.noreply.github.com>
2022-09-13 13:39:48 +0000

    improved styling

--- sharedUI/src/commonMain/kotlin/com/turbosokol/sharedfront/ui/BottomNavigationView.kt
+++ sharedUI/src/commonMain/kotlin/com/turbosokol/sharedfront/ui/BottomNavigationView.kt
@@ -2,4 +2,5 @@ package com.turbosokol.sharedfront.ui
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
-import andr
+import androidx.compose.material.*
+import and
