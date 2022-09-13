5a421e531eeb6f389c5d57854c52d53185b0573d
ijirak <119900896+ijirak@users.noreply.github.com>
2022-09-13 20:55:14 +0000

    more lint

--- sharedUI/src/commonMain/kotlin/com/turbosokol/sharedfront/ui/BottomNavigationView.kt
+++ sharedUI/src/commonMain/kotlin/com/turbosokol/sharedfront/ui/BottomNavigationView.kt
@@ -4,4 +4,5 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.fille
+import androidx.compose.material.icons.filled.*
+import androidx.compose.runtime.Composable
