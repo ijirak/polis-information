79b0dc7250bcc5da115ad2794bac2925c64c46f2
ijirak <119900896+ijirak@users.noreply.github.com>
2022-09-21 14:27:30 +0000

    Tidy up README

--- sharedUI/src/commonMain/kotlin/com/turbosokol/sharedfront/ui/BottomNavigationView.kt
+++ sharedUI/src/commonMain/kotlin/com/turbosokol/sharedfront/ui/BottomNavigationView.kt
@@ -9,4 +9,5 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.turbosokol.sharedfront.ui.scree
+import com.turbosokol.sharedfront.ui.screens.CalculatorScreen
+import com.turbos
