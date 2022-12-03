815bca38a279da58377e33bef9c092370b233745
ijirak <119900896+ijirak@users.noreply.github.com>
2022-12-03 20:57:39 +0000

    Fixed Warnings

--- sharedUI/src/commonMain/kotlin/com/turbosokol/sharedfront/ui/BottomNavigationView.kt
+++ sharedUI/src/commonMain/kotlin/com/turbosokol/sharedfront/ui/BottomNavigationView.kt
@@ -42,4 +42,7 @@ internal fun BottomNavigationView(viewModel: ApplicationViewModel, modifier: Mod
                             viewModel.selectedTab = tab
                         }
                     )
-     
+                }
+            }
+        }
+  
