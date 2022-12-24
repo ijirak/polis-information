48eaf5a9eb05f01e0b85b37bea8224e41c8f8647
ijirak <119900896+ijirak@users.noreply.github.com>
2022-12-24 06:20:33 +0000

    Updates README file

--- sharedUI/src/commonMain/kotlin/com/turbosokol/sharedfront/ui/MainView.kt
+++ sharedUI/src/commonMain/kotlin/com/turbosokol/sharedfront/ui/MainView.kt
@@ -2,4 +2,4 @@ package com.turbosokol.sharedfront.ui
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.turb
+import com.turbosokol.sharedfront.ui.theme.TradingAlchem
