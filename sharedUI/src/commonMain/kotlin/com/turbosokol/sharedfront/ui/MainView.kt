7690bae91783384f205614bbcb4c6ab91bd7fd26
ijirak <119900896+ijirak@users.noreply.github.com>
2022-12-24 15:43:47 +0000

    Hooks

--- sharedUI/src/commonMain/kotlin/com/turbosokol/sharedfront/ui/MainView.kt
+++ sharedUI/src/commonMain/kotlin/com/turbosokol/sharedfront/ui/MainView.kt
@@ -2,4 +2,5 @@ package com.turbosokol.sharedfront.ui
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.turbosokol.sharedfront.ui.theme.TradingAlchem
+import com.turbosokol.sharedfront.ui.theme.TradingAlchemyTheme
+import com.turbosokol.shared
