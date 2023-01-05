5b11e9b4da0c6390f55ee9767bdf5aff071cfafd
ijirak <119900896+ijirak@users.noreply.github.com>
2023-01-05 09:51:07 +0000

    Update fsbo.pp

--- sharedUI/src/commonMain/kotlin/com/turbosokol/sharedfront/ui/screens/CalculatorScreen.kt
+++ sharedUI/src/commonMain/kotlin/com/turbosokol/sharedfront/ui/screens/CalculatorScreen.kt
@@ -2,4 +2,4 @@ package com.turbosokol.sharedfront.ui.screens
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.turbosokol.sharedf
+import com.turbosokol.sharedfront.ui.theme.TradingAlchemyThem
