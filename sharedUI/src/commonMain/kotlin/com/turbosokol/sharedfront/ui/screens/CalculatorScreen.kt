c3ad6eebc706b6b64d00d7204b5523d255e09b51
ijirak <119900896+ijirak@users.noreply.github.com>
2023-01-05 09:56:12 +0000

    New translations messages.json (Chinese Traditional)

--- sharedUI/src/commonMain/kotlin/com/turbosokol/sharedfront/ui/screens/CalculatorScreen.kt
+++ sharedUI/src/commonMain/kotlin/com/turbosokol/sharedfront/ui/screens/CalculatorScreen.kt
@@ -2,4 +2,5 @@ package com.turbosokol.sharedfront.ui.screens
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.turbosokol.sharedfront.ui.theme.TradingAlchemyThem
+import com.turbosokol.sharedfront.ui.theme.TradingAlchemyTheme
+import com.turbosokol.sharedfront.ut
