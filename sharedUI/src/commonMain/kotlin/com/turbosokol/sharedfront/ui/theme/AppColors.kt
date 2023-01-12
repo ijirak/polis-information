47bdfebfcb23f58de67767a0ec10ef6ef62b247e
ijirak <119900896+ijirak@users.noreply.github.com>
2023-01-12 10:26:00 +0000

    final polish

--- sharedUI/src/commonMain/kotlin/com/turbosokol/sharedfront/ui/theme/AppColors.kt
+++ sharedUI/src/commonMain/kotlin/com/turbosokol/sharedfront/ui/theme/AppColors.kt
@@ -5,4 +5,4 @@ import androidx.compose.ui.graphics.Color
 object AppColors {
     val primary = Color(0xFF7de1c3)
     val secondary = Color(0xFF0014e6)
-    val seco
+    val secondaryLighter = Color(0xFF001
