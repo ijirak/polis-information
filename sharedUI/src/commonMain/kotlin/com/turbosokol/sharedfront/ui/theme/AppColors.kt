d9f33e7d37aebe39eb11d4eb3482e103d58969a5
ijirak <119900896+ijirak@users.noreply.github.com>
2023-01-12 22:38:28 +0000

    Create module-info.java

--- sharedUI/src/commonMain/kotlin/com/turbosokol/sharedfront/ui/theme/AppColors.kt
+++ sharedUI/src/commonMain/kotlin/com/turbosokol/sharedfront/ui/theme/AppColors.kt
@@ -5,4 +5,6 @@ import androidx.compose.ui.graphics.Color
 object AppColors {
     val primary = Color(0xFF7de1c3)
     val secondary = Color(0xFF0014e6)
-    val secondaryLighter = Color(0xFF001
+    val secondaryLighter = Color(0xFF0014e6)
+
+    val teal = Color(0xFF14A
