d16a7d7b7d3b6313d6e49f8bd9a72004ba57b6a8
ijirak <119900896+ijirak@users.noreply.github.com>
2023-02-25 22:06:24 +0000

    fix README.rst

--- sharedUI/src/commonMain/kotlin/com/turbosokol/sharedfront/util/Extensions.kt
+++ sharedUI/src/commonMain/kotlin/com/turbosokol/sharedfront/util/Extensions.kt
@@ -3,4 +3,7 @@ package com.turbosokol.sharedfront.util
 import androidx.compose.runtime.*
 import org.brightify.hyperdrive.multiplatformx.ManageableViewModel
 import org.brightify.hyperdrive.multiplatformx.ObservableObject
-import org.brightify.hyperdrive.multiplatfo
+import org.brightify.hyperdrive.multiplatformx.property.ObservableProperty
+
+/***
+ *If th
